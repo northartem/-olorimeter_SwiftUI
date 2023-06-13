920a42cab3165a4ee7db9fef9290a68c65cd731f
AssanCred <temix@live.ru>
2023-06-13 09:41:51 +0700

    Initial Commit

---
+++ Сolorimeter_SwiftUI/_olorimeter_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  _olorimeter_SwiftUIApp.swift
+//  Сolorimeter_SwiftUI
+//
+//  Created by Артём Латушкин on 13.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct _olorimeter_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
